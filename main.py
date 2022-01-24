0e8c8db8803a1ce6b51e8d1ca8626777d2c06f30
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-01-23 22:15:47 -0500

    Minute36.47
    Update to record progress1

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 class Item:
-    pass
+    pay_rate = 0.8 # the pay rate after 20 % discount
+
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Price {price} is not greater than or equal to zero!"
+
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+
+print(Item.__dict__) # all the attributes for Class Level
+print(item1.__dict__) # All the attributes for Instance Level.
 
