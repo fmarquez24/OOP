2924481f56e11c15c4bc4942081440c920371099
fernando <fmarquezofsjc@gmail.com>
2022-01-25 00:24:43 -0500

    Copy from jim

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import csv
 
 
 class Item:
-    pay_rate = 0.8  # the pay rate after 20 % discount
+    pay_rate = 0.8 # The pay rate after 20% discount
     all = []
-
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
         # Assign to self object
         self.name = name
@@ -33,12 +32,24 @@ class Item:
         for item in items:
             Item(
                 name=item.get('name'),
-                price=item.get('price'),
-                quantity=item.get('quantity'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
             )
 
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
-        return f"Item('{self.name}','{self.price}', '{self.quantity}')"
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
 Item.instantiate_from_csv()
