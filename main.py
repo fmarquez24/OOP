2c1fd6b168db92160a7b5efe47a1163427967ae5
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-01-25 18:27:12 -0500

    Fixed CSV?

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
     all = []
 
-    def __init__(self, name: str, price: float, quantity=0):
+    def __init__ (self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
