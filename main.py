61cebbd71c05cbdd7200b0ffc746bf1807d1b73b
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-01-24 21:29:17 -0500

    minuto 53
    getting nonetype

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import csv
+
+
 class Item:
-    pay_rate = 0.8 # the pay rate after 20 % discount
+    pay_rate = 0.8  # the pay rate after 20 % discount
     all = []
+
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -14,21 +18,28 @@ class Item:
         # Actions to execute
         Item.all.append(self)
 
-
     def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=item.get('price'),
+                quantity=item.get('quantity'),
+            )
+
     def __repr__(self):
         return f"Item('{self.name}','{self.price}', '{self.quantity}')"
 
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
-
+Item.instantiate_from_csv()
 print(Item.all)
