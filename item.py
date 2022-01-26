7448a5727e9881655a196153197f4263c8324d3b
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-01-25 21:15:45 -0500

    Minuto 1 :58

--- item.py
+++ item.py
@@ -13,11 +13,25 @@ class Item:
 
         # Assign to self object
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         # Actions to execute
         Item.all.append(self)
+
+    @property
+    def price(self):
+        return self.__price
+
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+
+    def apply_increment(self, increment_value):
+        self.__price = self.__price + self.__price * increment_value
+
+
+
+
 
     @property
     def name(self):
@@ -28,10 +42,8 @@ class Item:
         self.__name = value
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.__price * self.quantity
 
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
 
     @classmethod
     def instantiate_from_csv(cls):
@@ -58,4 +70,4 @@ class Item:
             return False
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.__price}, {self.quantity})"
