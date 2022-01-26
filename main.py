1757a5594e6b4b20245bd069711a35d4f1e1ee17
fmarquez24 <97994064+fmarquez24@users.noreply.github.com>
2022-01-25 21:37:57 -0500

    End of Video

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from item import Item
+#from phone import Phone
+from keyboard import Keyboard
 
-item1 = Item("MyItem", 750, 6)
+item1 = Keyboard("MyItem", 750, 6)
 
+item1.apply_discount()
+
+print(item1.price)
 item1.send_email()
--- phone.py
+++ phone.py
@@ -1,7 +1,7 @@
 from item import Item
 
 class Phone(Item):
-
+    pay_rate = 0.85
     def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
 
         # Call to super function to have access to all attributes /methods
